c808a21b4640132353b00e9ed10ba51cc8f5cb8f
Koichi Ishida <lingobuddies@gmail.com>
2011-05-26 12:20:58 -0700

    jobeet day 6

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -12,4 +12,18 @@
  */
 class JobeetCategory extends BaseJobeetCategory
 {
+  public function getActiveJobs($max = 10)
+  {
+    $q = Doctrine_Query::create()
+      ->from('JobeetJob j')
+      ->where('j.category_id = ?', $this->getId())
+      ->limit($max);
+   
+    return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+  }
+  
+  public function getSlug()
+  {
+    return Jobeet::slugify($this->getName());
+  }
 }
--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,13 +7,34 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetJobTable
-     */
-    public static function getInstance()
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object JobeetJobTable
+   */
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('JobeetJob');
+  }
+    
+  public function getActiveJobs(Doctrine_Query $q = null)
+  {
+    if (is_null($q))
     {
-        return Doctrine_Core::getTable('JobeetJob');
+      $q = Doctrine_Query::create()
+        ->from('JobeetJob j');
     }
+   
+    $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+      ->addOrderBy('j.expires_at DESC');
+   
+    return $q->execute();
+  }
+  
+  public function retrieveActiveJob(Doctrine_Query $q)
+  {
+    $q->andWhere('a.expires_at > ?', date('Y-m-d H:i:s', time()));
+ 
+    return $q->fetchOne();
+  }
 }
